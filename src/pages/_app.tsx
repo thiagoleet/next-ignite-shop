442b0024b91a022787ff5ec82a9f5501744eb9ea
Thiago Leite <thiagofmleite@gmail.com>
2025-01-06 20:19:17 -0300

    Replace logo image tag with Next.js Image component for optimized loading

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import { globalStyles } from "@/styles/global";
 import { AppProps } from "next/app";
+import Image from "next/image";
 import logoImg from "../assets/logo.svg";
 import { Container, Header } from "@/styles/pages/app";
 
@@ -10,8 +11,8 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <Container>
       <Header>
-        <img
-          src={logoImg.src}
+        <Image
+          src={logoImg}
           alt="Logo"
         />
       </Header>
