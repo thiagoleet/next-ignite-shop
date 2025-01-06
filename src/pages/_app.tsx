9e4ebd6a625f3feed9fb5b3b21d992ed7f6266d2
Thiago Leite <thiagofmleite@gmail.com>
2025-01-06 20:11:51 -0300

    Integrate logo into app layout with styled container and header

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,21 @@
 import { globalStyles } from "@/styles/global";
 import { AppProps } from "next/app";
+import logoImg from "../assets/logo.svg";
+import { Container, Header } from "@/styles/pages/app";
 
 // Applying the global styles
 globalStyles();
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <Container>
+      <Header>
+        <img
+          src={logoImg.src}
+          alt="Logo"
+        />
+      </Header>
+      <Component {...pageProps} />
+    </Container>
+  );
 }
