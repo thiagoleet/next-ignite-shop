d04746cf6581d3a424f653c8865e7ad14a776670
Thiago Leite <thiagofmleite@gmail.com>
2025-01-07 15:19:30 -0300

    Implement product page with dynamic data fetching and improved layout

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,31 +1,83 @@
+import { GetStaticPaths, GetStaticProps } from "next";
+import Image from "next/image";
+import Stripe from "stripe";
 import {
   ImageContainer,
   ProductContainer,
   ProductDetails,
 } from "@/styles/pages/product";
+import { stripe } from "@/lib/stripe";
 import { useRouter } from "next/router";
 
-export default function ProductPage() {
-  const { query } = useRouter();
+interface ProductProps {
+  product: {
+    id: string;
+    name: string;
+    imageUrl: string;
+    price: string;
+    description: string;
+  };
+}
+
+export default function ProductPage({ product }: ProductProps) {
+  const { isFallback } = useRouter();
+
+  if (isFallback) {
+    return <h1>Carregando...</h1>;
+  }
 
   return (
     <ProductContainer>
-      <ImageContainer></ImageContainer>
+      <ImageContainer>
+        <Image
+          src={product.imageUrl}
+          alt={product.name}
+          width={520}
+          height={480}
+        />
+      </ImageContainer>
       <ProductDetails>
-        <h1>Camisa Titulo</h1>
-        <span>R$ 99,99</span>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
-          vehicula, libero ut aliquam lacinia, metus nunc fermentum enim, nec
-          varius purus risus nec purus. Aenean nec nisl nec nisl ultricies
-          fermentum. Nullam auctor, erat nec aliquam tempus, nunc lorem
-          sollicitudin turpis, nec aliquam erat nunc a purus. Integer nec
-          fermentum est. Nullam auctor, erat nec aliquam tempus, nunc lorem
-          sollicitudin turpis, nec aliquam erat nunc a purus. Integer nec
-          fermentum est.
-        </p>
+        <h1>{product.name}</h1>
+        <span>{product.price}</span>
+        <p>{product.description}</p>
         <button>Comprar agora</button>
       </ProductDetails>
     </ProductContainer>
   );
 }
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  return {
+    paths: [],
+    fallback: true,
+  };
+};
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export const getStaticProps: GetStaticProps<any, { id: string }> = async ({
+  params,
+}) => {
+  const productId = params?.id;
+
+  const product = await stripe.products.retrieve(productId as string, {
+    expand: ["default_price"],
+  });
+
+  const price = product.default_price as Stripe.Price;
+
+  return {
+    props: {
+      product: {
+        id: product.id,
+        name: product.name,
+        imageUrl: product.images[0],
+        price: new Intl.NumberFormat("pt-BR", {
+          style: "currency",
+          currency: "BRL",
+        }).format(price.unit_amount ? price.unit_amount / 100 : 0),
+        description: product.description,
+      },
+    },
+    revalidate: 60 * 60 * 1, // 1 hour
+  };
+};
