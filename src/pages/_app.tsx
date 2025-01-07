2da1a0c657b3a3dd71eba06a42bc14dab775de65
Thiago Leite <thiagofmleite@gmail.com>
2025-01-07 16:17:38 -0300

    Wrap logo image in a link for improved navigation to the homepage

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,9 @@
-import { globalStyles } from "@/styles/global";
 import { AppProps } from "next/app";
+import Link from "next/link";
 import Image from "next/image";
-import logoImg from "../assets/logo.svg";
+import { globalStyles } from "@/styles/global";
 import { Container, Header } from "@/styles/pages/app";
+import logoImg from "../assets/logo.svg";
 
 // Applying the global styles
 globalStyles();
@@ -11,10 +12,12 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <Container>
       <Header>
-        <Image
-          src={logoImg}
-          alt="Logo"
-        />
+        <Link href="/">
+          <Image
+            src={logoImg}
+            alt="Logo"
+          />
+        </Link>
       </Header>
       <Component {...pageProps} />
     </Container>
