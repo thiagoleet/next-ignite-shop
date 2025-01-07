07ecbc9da7469540f9270dcef870a7c87f7b5650
Thiago Leite <thiagofmleite@gmail.com>
2025-01-07 17:03:16 -0300

    Enhance success page with user feedback and styling

--- src/pages/success.tsx
+++ src/pages/success.tsx
@@ -1,3 +1,18 @@
+import { ImageContainer, SuccessContainer } from "@/styles/pages/sucess";
+import Link from "next/link";
+
 export default function Success() {
-  return <h1>Success</h1>;
+  return (
+    <SuccessContainer>
+      <h1>Compra efetuada!</h1>
+      <ImageContainer></ImageContainer>
+
+      <p>
+        Uhull <strong>USUARIO</strong>, sua compra <strong>PRODUTO</strong> já
+        está à caminho da sua casa!
+      </p>
+
+      <Link href="/">Voltar ao catálogo</Link>
+    </SuccessContainer>
+  );
 }
